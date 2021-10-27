893905e9558f2a6fdba477a17dba273ab093de36
amishah04 <87444716+amishah04@users.noreply.github.com>
2021-10-26 19:39:16 -0700

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -4,6 +4,17 @@ public class Welcome {
    
        System.out.println("Hello this is welcome program");
        System.out.println("I made some changes to the program")
+       String Fname = "Hello";
+       String Lname = " World";
+      
+      System.out.println(Fname + " " + Lname);
+      
+      
+      
+      
+      
+          
+          
    }
    
 }
